564a0b7cbcbb706b24b31f4c42ad0b6510c75e4b
dino <853145189@qq.com>
2019-04-26 11:12:37 +0800

    增加版本号检测功能 & 优化代码逻辑

--- command/delete.js
+++ command/delete.js
@@ -1,7 +1,7 @@
 module.exports=()=>{
   const inquirer = require('inquirer')
   const chalk = require('chalk');
-  const fs = require('fs');
+  const rm = require('rimraf')
   inquirer.prompt([{
     type: 'input',
     message: '请输入要删除的项目名称：',
@@ -16,32 +16,11 @@ module.exports=()=>{
   }
   ]).then(res=> {
     if(res.delete){
-      delDir(res.projectName)
+      rm(res.projectName,[],()=>{
+        console.log(chalk.blue(`文件夹${res.projectName}删除成功`));
+      })
     }else{
       console.log(chalk.red('取消删除'));
     }
-    
   })
-
-  
-  
-  //循环删除文件和文件夹
-  function delDir(path){
-    let files = [];
-    if(fs.existsSync(path)){
-        files = fs.readdirSync(path);
-        files.forEach((file, index) => {
-            let curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()){
-                delDir(curPath); //递归删除文件夹
-            } else {
-                fs.unlinkSync(curPath); //删除文件
-            }
-        });
-        fs.rmdirSync(path);
-        console.log(chalk.blue(`文件夹${path}删除成功`));
-    }else{
-      console.log(chalk.red('目录不存在'));
-    }
-  }
 }
--- command/init.js
+++ command/init.js
@@ -1,29 +1,46 @@
 module.exports=()=>{
+  const path = require("path")
+  const exists = require('fs').existsSync
+  const chalk = require('chalk');
   const download = require('download-git-repo'); //通过git仓库拉取
   const inquirer = require('inquirer')
-  const process = require('child_process'); //引入子进程模块
+  const proce = require('child_process'); //引入子进程模块
   const ora = require('ora');   //ora用来提升用户体验
   const source = 'wangminghuan/bootstrap-static-ui';
   const spinner = ora(`正在下载项目模板，源地址为：https://github.com/${source}.git`);
   const spinnerInstall = ora(`正在安装依赖，请稍后`);
-  inquirer.prompt([{
-    type: 'input',
-    message: '请输入项目名称：',
-    name: 'projectName',
-    default: "my-template-project", // 默认值
-    validate: function(val) {
-      if(!val) { // 校验位数
-        return "请输入有效项目名称！";
-      }else{
-        return true
-      }
-    }
-  }]).then(res=> {
-    spinner.start();
-    const target = res.projectName;
-    downHub(target)
+  const checkVersion = require('../lib/check-version')
+  //检测cli版本是否更新到最新版
+  checkVersion(() => {
+    done();
   })
-   
+  //开始执行
+  function done(){
+    inquirer.prompt([{
+      type: 'input',
+      message: '请输入项目名称：',
+      name: 'projectName',
+      default: "my-template-project", // 默认值
+      validate: function(val) {
+        if(!val) { // 校验位数
+          return "请输入有效项目名称！";
+        }else{
+          return true
+        }
+      }
+    }]).then(res=> {
+      const target = res.projectName;
+      const to = path.join(process.cwd(),target)
+      if(!exists(to)){
+        spinner.start();
+        downHub(target)
+      }else{
+        console.log(chalk.red("目录已存在，请重新输入目录"))
+        done();
+      }
+    })
+  }
+  
   //拉取仓库代码
   function downHub(target){
      new Promise(function(resolve, reject) {
@@ -48,7 +65,7 @@ module.exports=()=>{
    //打开文件夹，安装依赖
    function openAndInstallDep(target){
     spinnerInstall.start();
-    process.exec(`cd ${target} && npm install`,function (error, stdout, stderr) {
+    proce.exec(`cd ${target} && npm install`,function (error, stdout, stderr) {
       if (error !== null) {
         spinnerInstall.fail('exec error: ' + error);
       }else{
